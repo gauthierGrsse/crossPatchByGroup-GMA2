571fc5991d008b27eb79d1de118dbb80b4dac3fe
Gauthier G <gguerisse@icloud.com>
2021-12-29 11:31:25 -0600

    swap or take patch complet

--- main.lua
+++ main.lua
@@ -1,12 +1,13 @@
 -- Gauthier G.
 -- v0.1
 -- CrossPatchByGroup
-
 -- Shortcut
 local cmd = gma.cmd
 local setvar = gma.show.setvar
 local getvar = gma.show.getvar
-local sleep = gma.sleep
+local function sleep()
+    gma.sleep(0.1)
+end
 local confirm = gma.gui.confirm
 local msgbox = gma.gui.msgbox
 local textinput = gma.textinput
@@ -44,6 +45,15 @@ local function length(tableau)
     return count
 end
 
+local function inList(tableau, element)
+    for key, value in pairs(tableau) do
+        if value == element then
+            return true
+        end
+    end
+    return false
+end
+
 local function inputList(name)
     local list = {}
     local running = true
@@ -59,11 +69,9 @@ local function inputList(name)
     return list
 end
 
-local function start(argCmd)
-    local group = textinput("Group", "0")
-
+local function findFixtureInGroup(group)
     -- Récupération du fichier XML du groupe
-    cmd("Export Group "..group..' \"tempgroup\" /nc')
+    cmd("Export Group " .. group .. ' \"tempgroup\" /nc')
     local fichierPath = gma.show.getvar('PATH') .. '/importexport/tempgroup.xml'
 
     -- Ouverture du fichier et stockage dans une variable lua
@@ -72,15 +80,116 @@ local function start(argCmd)
     local textXML = io.read("*all")
     io.close(fichier)
 
-    -- Recherche des fixture ID
-    local tempPos = string.find(textXML, 'fix_id="') + 1    -- position de la fixture ID
-    echo(tempPos)
-    local tempText = string.sub(textXML, tempPos + 7, tempPos + 11)    -- on récupère le fixture ID
-    echo(tempText)
-    tempPos = string.find(tempText, '"')    -- on recupère la position du " de fin pour nettoyer
-    echo(tempPos)
-    tempText = string.sub(tempText, 0, tempPos - 1) -- on récupère tout avant le "
-    feedback(tempText)
+    -- Creation table groupe
+    local fixtureGroup = {}
+
+    -- Lecture du fichier pour trouver les fixture ID
+    local cursorPos = 1 -- Curseur de lecture pour pas lire 2 fois la même chose
+    local running = true
+    while running == true do
+        if string.find(textXML, 'fix_id="', cursorPos) then
+            local tempPos = string.find(textXML, 'fix_id="', cursorPos) + 1 -- position de la ligne fixture ID
+            cursorPos = tempPos + 1 -- on déplace le curseur apres pour ne pas lire cet ID 2 fois
+            local tempText = string.sub(textXML, tempPos + 7, tempPos + 12) -- on récupère le fixture ID
+            tempPos = string.find(tempText, '"') -- on recupère la position du " de fin pour nettoyer la merde au bout
+            tempText = string.sub(tempText, 0, tempPos - 1) -- on récupère tout avant le "
+            echo("Find fixture ID " .. tempText .. " in group " .. group)
+            tempText = tonumber(tempText)
+            fixtureGroup[length(fixtureGroup) + 1] = tempText -- On ajoute le fixture ID dans le tableau
+        else
+            running = false
+        end
+    end
+
+    return fixtureGroup
+end
+
+local function getPatchOfFixture(fixtureID)
+    return property.get(getobj.child(getobj.handle("Fixture " .. fixtureID), 0), 4)
+end
+
+local function patchFixture(fixtureID, DmxAdress)
+    cmd('Assign Fixture ' .. fixtureID .. ' At Dmx ' .. DmxAdress .. ' /nc')
+end
+
+local function start(argCmd)
+    local groupAIndex = textinput("Group A", "0")
+    local groupBIndex = textinput("Group B", "0")
+
+    local groupAFixtures = findFixtureInGroup(groupAIndex)
+    local groupBFixtures = findFixtureInGroup(groupBIndex)
+
+    if length(groupAFixtures) ~= length(groupBFixtures) then -- Test si les 2 groupes font la meme longueur, sinon erreur
+        error("Le groupe A et B ne comporte pas le meme nombre de fixture, cross patch impossible !")
+        return false
+    end
+
+    local groupAPatch = {} -- creation du talbeau patch groupe A
+    for key, value in pairs(groupAFixtures) do
+        groupAPatch[length(groupAPatch) + 1] = getPatchOfFixture(value)
+    end
+
+    local groupBPatch = {} -- creation du talbeau patch groupe B
+    for key, value in pairs(groupBFixtures) do
+        groupBPatch[length(groupBPatch) + 1] = getPatchOfFixture(value)
+    end
+
+    -- Affichage d'un recap global du swap
+    gma.feedback("--------------------------------------------------------------------------------------")
+    for key, value in pairs(groupAFixtures) do
+        if inList(groupBFixtures, value) then
+            gma.feedback("Fixture ID " .. value .. " patch " .. groupAPatch[key] ..
+                             " <------- TAKE PATCH OF -------< Fixture ID " .. groupBFixtures[key] .. " patch " ..
+                             groupBPatch[key])
+        else
+            gma.feedback("Fixture ID " .. value .. " patch " .. groupAPatch[key] ..
+                             " <------- SWAP PATCH -------> Fixture ID " .. groupBFixtures[key] .. " patch " ..
+                             groupBPatch[key])
+        end
+    end
+    gma.feedback("--------------------------------------------------------------------------------------")
+
+    if confirm("CrossPatchByGroup",
+        "Confirmez le cross patch entre le groupe " .. groupAIndex .. " et le groupe " .. groupBIndex .. " ?") then -- demande confirmation
+        local patchedList = {} -- Liste des deja patchee
+
+        local progressBar = progress.start("Patch group A") -- Affichage progress bar pour faire joli
+        progress.setrange(progressBar, 0, length(groupAFixtures))
+        local progressBarIndex = 0
+
+        for key, value in pairs(groupAFixtures) do -- Patch du groupe A
+            patchFixture(value, groupBPatch[key])
+            patchedList[length(patchedList) + 1] = value -- Ajout de la fixture a la liste des deja patchee
+
+            progressBarIndex = progressBarIndex + 1
+            progress.set(progressBar, progressBarIndex)
+            progress.settext(progressBar, "Fixture " .. value)
+            sleep()
+        end
+        progress.stop(progressBar)
+
+        progressBar = progress.start("Patch group A") -- Affichage progress bar pour faire joli
+        progress.setrange(progressBar, 0, length(groupAFixtures))
+        progressBarIndex = 0
+
+        for key, value in pairs(groupBFixtures) do -- Patch du groupe B
+            if inList(patchedList, value) then -- Test si fixture pas deja patchee
+                feedback("Fixture " .. value .. " deja patchee")
+            else
+                patchFixture(value, groupAPatch[key])
+                patchedList[length(patchedList) + 1] = value -- Ajout de la fixture a la liste des deja patchee
+            end
+
+            progressBarIndex = progressBarIndex + 1
+            progress.set(progressBar, progressBarIndex)
+            progress.settext(progressBar, "Fixture " .. value)
+            sleep()
+        end
+        progress.stop(progressBar)
+
+    else
+        feedback("Cross patch annule")
+    end
 end
 
 return start
